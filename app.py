20acd39a67e076c8b74ee6b9bd5084592bc505c3
VathareVinayak <vinayak.vathare2004@gmail.com>
2025-02-02 19:53:11 +0530

    API Created , All Frontend Done , Database is Connected with the requirement files

--- app.py
+++ app.py
@@ -1,17 +1,75 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request , redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///records.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/")
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer , primary_key=True)
+    title = db.Column(db.String(200) , nullable=False) 
+    desc = db.Column(db.String(500) , nullable=False)
+    date_created = db.Column(db.DateTime , default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+
+@app.route("/" , methods=["GET" , "POST"])
 def hello_world():
-    return render_template("index.html")
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+        
+        todo = Todo(title = title , desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    
+    allTodo= Todo.query.all()
+    return render_template("index.html" , allTodo=allTodo)
+
+@app.route("/shows")
+def get_all_todos():
+    allTodo= Todo.query.all()
+    print(allTodo)
+    
+    return "this is the project"
+    
+
+@app.route("/delete/<int:sno>")
+def delete_task(sno):  
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/update/<int:sno>" , methods=["GET" , "POST"])
+def update_task(sno):
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    
+    
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html" , todo=todo)
 
 
-
-
-
-
-
+# ✅ Create the database file when the app starts
+with app.app_context():
+    db.create_all()
+    print("Database created successfully!")
 
 
 
